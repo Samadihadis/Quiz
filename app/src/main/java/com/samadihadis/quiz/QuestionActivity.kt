48b4c58125ed2fd31eba2dfc6e14ef82492bafe5
h.samadi <samadi.hadis@gmail.com>
2023-09-26 12:47:50 +0330

    Use Typeface & ContextCompat Completing ProgressBar

--- app/src/main/java/com/samadihadis/quiz/QuestionActivity.kt
+++ app/src/main/java/com/samadihadis/quiz/QuestionActivity.kt
@@ -1,8 +1,13 @@
 package com.samadihadis.quiz
 
+import android.graphics.Color
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
 import com.samadihadis.quiz.databinding.ActivityMainBinding
 import com.samadihadis.quiz.databinding.ActivityQuestionBinding
 
@@ -10,23 +15,59 @@ class QuestionActivity : AppCompatActivity() {
 
 
     lateinit var binding: ActivityQuestionBinding
+    var currentQuestion = 0
+    lateinit var questionList: ArrayList<Question>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityQuestionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        questionList = Constants.getQuestions()
+
+        val question = questionList[currentQuestion]
+        loadQuestionData(question)
     }
 
+    fun loadQuestionData(question: Question) {
 
-    private fun setQuestion(question: Question){
-        with(binding) {
-            titleTextView.text = question.question
-            countryImageView.setImageResource(question.image)
-            optionOneTextView.text = question.optionOne
-            optionTwoTextView.text = question.optionTwo
-            optionThreeTextView.text = question.optionThree
-            optionFourTextView.text = question.optionFour
+        binding.titleTextView.text = question.question
+        binding.countryImageView.setImageResource(question.image)
+        binding.optionOneTextView.text = question.optionOne
+        binding.optionTwoTextView.text = question.optionTwo
+        binding.optionThreeTextView.text = question.optionThree
+        binding.optionFourTextView.text = question.optionFour
+        binding.questionProgressBar.progress = currentQuestion + 1
+
+
+        binding.stepTextView.text = "${currentQuestion + 1} / ${binding.questionProgressBar.max}"
+
+
+    }
+
+    fun setDefaultForOption() {
+        var textViewOptions = ArrayList<TextView>()
+        textViewOptions.add(binding.optionOneTextView)
+        textViewOptions.add(binding.optionTwoTextView)
+        textViewOptions.add(binding.optionThreeTextView)
+        textViewOptions.add(binding.optionFourTextView)
+
+        for (textViewOption in textViewOptions) {
+            textViewOption.typeface = Typeface.DEFAULT
+
+            textViewOption.background =
+                ContextCompat.getDrawable(this, R.drawable.option_background_selected)
+            textViewOption.setTextColor(Color.parseColor("#000000"))
         }
+    }
+
+    fun onOptionsClicked(view: View) {
+        setDefaultForOption()
+        var selectedTextView = view as TextView
+        selectedTextView.typeface = Typeface.DEFAULT_BOLD
+        selectedTextView.background =
+            ContextCompat.getDrawable(this, R.drawable.option_background_selected)
+        selectedTextView.setTextColor(Color.parseColor("#000000"))
+
     }
 }
